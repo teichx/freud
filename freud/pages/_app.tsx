86590d583241615ddb43500ab8e0cf1d730bd9f5
teichx <teichx@hotmail.com>
2023-01-12 23:49:36 -0300

    feat: Prepare for Theme

--- freud/pages/_app.tsx
+++ freud/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AuthContextProvider } from 'context/Auth';
 import type { AppProps } from 'next/app';
+import { CustomTheme } from 'themes/CustomTheme';
 
 export const App = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider>
+  <ChakraProvider theme={CustomTheme}>
     <AuthContextProvider>
       <Component {...pageProps} />
     </AuthContextProvider>
