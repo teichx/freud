1c529f91b49851193268ddcb0be21b649dfcdb0d
teichx <teichx@hotmail.com>
2023-01-17 22:19:22 -0300

    feat: Logo use custom font

--- freud/components/Logo/Logo.tsx
+++ freud/components/Logo/Logo.tsx
@@ -1,18 +1,23 @@
 import { FC } from 'react';
 
-import { Box, HStack, Text } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
 
+import { LogoText } from './styles';
 import { LogoProps, LogoVariants } from './types';
 
 const ICON_SIZE = {
   small: 24,
   medium: 48,
   large: 64,
-};
+} as const;
 
 const VARIANTS_WITH_ICON: LogoVariants[] = ['icon', 'full'];
 
 const VARIANTS_WITH_TEXT: LogoVariants[] = ['text', 'full'];
+
+const i18nText = {
+  logo: 'Freud',
+} as const;
 
 export const Logo: FC<LogoProps> = ({
   variant = 'full',
@@ -25,14 +30,12 @@ export const Logo: FC<LogoProps> = ({
         <Box
           w={`${ICON_SIZE[size]}px`}
           h={`${ICON_SIZE[size]}px`}
-          bg='#cc1316'
+          bg='book.desertSun'
         />
       )}
 
       {VARIANTS_WITH_TEXT.includes(variant) && (
-        <Box>
-          <Text>Freud</Text>
-        </Box>
+        <LogoText>{i18nText.logo}</LogoText>
       )}
     </HStack>
   </Box>
---
+++ freud/components/Logo/styles.ts
@@ -0,0 +1,7 @@
+import { Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const LogoText = styled(Text)({
+  fontFamily: "'Expletus Sans', cursive;",
+  fontSize: '32px',
+});
