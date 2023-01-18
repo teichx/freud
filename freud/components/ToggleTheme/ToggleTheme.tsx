19226d53da7a7161ca5c077b4b23c2b9737f6e17
teichx <teichx@hotmail.com>
2023-01-17 23:13:47 -0300

    feat: Tooltip in ToggleTheme

--- freud/components/ToggleTheme/ToggleTheme.tsx
+++ freud/components/ToggleTheme/ToggleTheme.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
 
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { IconButton, IconButtonProps, useColorMode } from '@chakra-ui/react';
+import {
+  IconButton,
+  IconButtonProps,
+  Tooltip,
+  useColorMode,
+} from '@chakra-ui/react';
+
+const ptBR = {
+  toDark: 'Usar tema escuro',
+  toLight: 'Usar tema claro',
+} as const;
 
 export const ToggleTheme: FC<
   Omit<IconButtonProps, 'onClick' | 'icon' | 'aria-label'>
@@ -10,13 +20,15 @@ export const ToggleTheme: FC<
   const isDark = colorMode === 'dark';
 
   return (
-    <IconButton
-      {...props}
-      bg='whiteAlpha.400'
-      color='book.desertSun'
-      aria-label='Toggle color mode'
-      onClick={toggleColorMode}
-      icon={isDark ? <SunIcon /> : <MoonIcon />}
-    />
+    <Tooltip label={isDark ? ptBR.toLight : ptBR.toDark}>
+      <IconButton
+        {...props}
+        bg='whiteAlpha.400'
+        color='book.desertSun'
+        aria-label='Toggle color mode'
+        onClick={toggleColorMode}
+        icon={isDark ? <SunIcon /> : <MoonIcon />}
+      />
+    </Tooltip>
   );
 };
