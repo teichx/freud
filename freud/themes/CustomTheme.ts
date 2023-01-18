4391bec07972df6229ad161f0e8f665efdae1d7b
teichx <teichx@hotmail.com>
2023-01-17 21:49:26 -0300

    feat: Provide default colors and type for theme

--- freud/themes/CustomTheme.ts
+++ freud/themes/CustomTheme.ts
@@ -1,3 +1,20 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeOverride } from '@chakra-ui/react';
 
-export const CustomTheme = extendTheme({});
+const theme = {
+  colors: {
+    book: {
+      darkBlue: '#08153A',
+      navyBlue: '#0B1F65',
+      royalBlue: '#2535D9',
+      desertSun: '#C08D2C',
+    },
+  },
+} as const;
+
+export const CustomTheme = extendTheme(theme);
+
+export type CustomThemeProps = typeof theme;
+
+export type ThemeProps = ThemeOverride & CustomThemeProps;
+
+export type HavingThemeProps = { theme?: CustomThemeProps };
