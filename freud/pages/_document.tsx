eacff67eb3cc2b125beaf37faa2a98a36fe88115
teichx <teichx@hotmail.com>
2023-01-17 21:49:56 -0300

    feat: Get font for Logo

--- freud/pages/_document.tsx
+++ freud/pages/_document.tsx
@@ -4,6 +4,14 @@ export const Document = () => (
   <Html lang='en'>
     <Head />
     <script src='https://accounts.google.com/gsi/client' async defer />
+
+    <link rel='preconnect' href='https://fonts.googleapis.com' />
+    <link rel='preconnect' href='https://fonts.gstatic.com' />
+    <link
+      href='https://fonts.googleapis.com/css2?family=Expletus+Sans:wght@600&display=swap'
+      rel='stylesheet'
+    />
+
     <body>
       <Main />
       <NextScript />
