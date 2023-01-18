9f24febc62dce34d9d76595b7d3629c94ebe6c48
teichx <teichx@hotmail.com>
2023-01-18 10:21:11 -0300

    feat: Create AppPage template

---
+++ freud/template/app/AppPage/AppPage.tsx
@@ -0,0 +1,35 @@
+import { FC, PropsWithChildren, useEffect } from 'react';
+
+import { Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+import { useAuth } from '~/services/Auth';
+
+import { Header } from '../../../sections/Header';
+
+export const AppPage: FC<PropsWithChildren> = ({ children }) => {
+  const { isLogged } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLogged) return;
+
+    router.push('/app/login');
+  }, [isLogged, router]);
+
+  return isLogged ? (
+    <Box>
+      <Header />
+
+      <Box
+        px={{
+          base: 4,
+          md: 8,
+        }}
+        py='4'
+      >
+        {children}
+      </Box>
+    </Box>
+  ) : null;
+};
