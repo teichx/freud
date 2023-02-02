04ded161ba1d3cf46cf0d26e6d72f1df590c9a08
teichx <teichx@hotmail.com>
2023-02-01 23:57:21 -0300

    feat: Index with GoogleLogin button

--- freud/pages/index.tsx
+++ freud/pages/index.tsx
@@ -1,3 +1,17 @@
-export const Index = () => <></>;
+import { Box } from '@chakra-ui/react';
+
+import { GoogleLogin } from '~/components/GoogleLogin';
+
+export const Index = () => (
+  <Box
+    height='100vh'
+    width='100vw'
+    display='flex'
+    alignItems='center'
+    justifyContent='center'
+  >
+    <GoogleLogin radius='large' />
+  </Box>
+);
 
 export default Index;
