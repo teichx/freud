efbe5c0a0eeead232e718f45ee68b6a9ba2b9d12
teichx <teichx@hotmail.com>
2023-03-10 20:21:46 -0300

    refactor: Change style for Header Button

--- src/core/sections/Header/styles.ts
+++ src/core/sections/Header/styles.ts
@@ -1,6 +1,19 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Theme } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
-export const HeaderButton = styled(Button)({
-  height: 'auto',
-});
+export const HeaderButton = styled(Button)(
+  ({ selected, theme }: { theme?: Theme; selected: boolean }) => ({
+    height: 'auto',
+    paddingLeft: theme?.space[4],
+    paddingRight: theme?.space[4],
+    paddingTop: theme?.space[2],
+    paddingBottom: theme?.space[2],
+    borderRadius: 0,
+    backgroundColor: selected
+      ? theme?.colors.whiteAlpha[300]
+      : theme?.colors.transparent,
+    ':hover': {
+      backgroundColor: theme?.colors.whiteAlpha[400],
+    },
+  })
+);
