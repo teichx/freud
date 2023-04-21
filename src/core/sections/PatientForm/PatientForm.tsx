ce9b9fc75a5d4102c2c44ad7570d242e2ef029a1
teichx <teichx@hotmail.com>
2023-04-21 01:15:57 -0300

    refactor: PatientForm with Form wrapper

--- src/core/sections/PatientForm/PatientForm.tsx
+++ src/core/sections/PatientForm/PatientForm.tsx
@@ -1,15 +1,30 @@
+import { Form } from '@unform/web';
+
 import { ComplainedCheck } from './ComplainedCheck';
 import { ComplainedHistory } from './ComplainedHistory';
 import { FirstConsult } from './FirstConsult';
 import { FreeText } from './FreeText';
 import { PersonalData } from './PersonalData';
 
-export const PatientForm = () => (
-  <div>
-    <PersonalData />
-    <FirstConsult />
-    <ComplainedHistory />
-    <ComplainedCheck />
-    <FreeText />
-  </div>
-);
+export const PatientForm = () => {
+  const handleSubmit = (data: unknown) => {
+    console.log(data);
+  };
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      initialData={{
+        name: 'foo',
+        cpf: '12233344422',
+      }}
+    >
+      <button type='submit'>enviar</button>
+      <PersonalData />
+      <FirstConsult />
+      <ComplainedHistory />
+      <ComplainedCheck />
+      <FreeText />
+    </Form>
+  );
+};
