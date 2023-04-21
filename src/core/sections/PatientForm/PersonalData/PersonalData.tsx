4aa0f570cd3ce4a42c1863b83712008759a63bea
teichx <teichx@hotmail.com>
2023-04-21 11:12:39 -0300

    feat: Add masks on personal data in patients form

--- src/core/sections/PatientForm/PersonalData/PersonalData.tsx
+++ src/core/sections/PatientForm/PersonalData/PersonalData.tsx
@@ -15,7 +15,11 @@ export const PersonalData = () => {
         <FormText name='name' label={t('name')} isRequired />
 
         <Box maxW='100px'>
-          <FormText name='age' label={t('age')} />
+          <FormText
+            name='age'
+            label={t('age')}
+            mask={{ mask: Number, min: 1, max: 150 }}
+          />
         </Box>
         <Box w='300px'>
           <FormText name='gender' label={t('gender')} />
@@ -24,15 +28,28 @@ export const PersonalData = () => {
 
       <Stack columnGap='4' direction={['column', 'row']}>
         <FormText name='profession' label={t('profession')} />
-        <FormText name='cpf' label={t('cpf')} />
-        <FormText name='rg' label={t('rg')} />
+        <FormText
+          name='cpf'
+          label={t('cpf')}
+          mask={{ mask: '000.000.000.00' }}
+        />
+        <FormText name='rg' label={t('rg')} mask={{ mask: '00.000.000' }} />
         <FormText name='schooling' label={t('schooling')} />
         <FormText name='marriage_status' label={t('marriage_status')} />
       </Stack>
 
       <Stack columnGap='4' direction={['column', 'row']}>
         <FormText name='address' label={t('address')} />
-        <FormText name='phone_number' label={t('phone_number')} />
+        <FormText
+          name='phone_number'
+          label={t('phone_number')}
+          mask={{
+            mask: [
+              { mask: '(00) 0000-0000', lazy: false },
+              { mask: '(00) 0 0000-0000' },
+            ],
+          }}
+        />
         <FormText name='emergency' label={t('emergency')} />
       </Stack>
     </Section>
