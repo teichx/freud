6bbb5ed17ff990ab1140d2939eddb5c121b46e56
teichx <teichx@hotmail.com>
2023-04-24 10:09:58 -0300

    feat: Create types for patient form

---
+++ src/core/sections/PatientForm/types.ts
@@ -0,0 +1,54 @@
+import { marriage_status, schooling } from '~/core/locales/options';
+
+import {
+  COGNITIVE_FIELDS,
+  FREE_TEXT_FIELDS,
+  FIRST_CONSULT_FIELDS,
+  COMPLAINED_HISTORY_FIELDS,
+} from './constants';
+
+export type PersonalDataFields = {
+  name: string;
+  age?: number;
+  gender?: string;
+  profession?: string;
+  cpf?: string;
+  rg?: string;
+  schooling?: (typeof schooling)[number];
+  marriage_status?: (typeof marriage_status)[number];
+  address?: string;
+  phone_number?: string;
+  emergency?: string;
+};
+
+export type FirstConsultFields = {
+  [key in (typeof FIRST_CONSULT_FIELDS)[number]]?: string;
+};
+
+export type ComplainedHistoryFields = {
+  [key in (typeof COMPLAINED_HISTORY_FIELDS)[number]]?: string;
+};
+
+export type CognitiveFields = {
+  cognitive: { other?: string } & {
+    [key in (typeof COGNITIVE_FIELDS.cognitive)[number]]?: boolean;
+  };
+  emotional: { other?: string } & {
+    [key in (typeof COGNITIVE_FIELDS.emotional)[number]]?: boolean;
+  };
+};
+
+export type FreeTextFieldFields = {
+  [key in (typeof FREE_TEXT_FIELDS)[number]]?: string;
+};
+
+export type PatientMetaFields = {
+  archived_at: string;
+};
+
+export type PatientFields = PersonalDataFields &
+  FirstConsultFields &
+  ComplainedHistoryFields &
+  CognitiveFields &
+  FreeTextFieldFields &
+  PatientMetaFields;
