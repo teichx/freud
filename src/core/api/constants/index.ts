b4d40020956d32e18d3aaf7f459a0041e4f490ac
teichx <teichx@hotmail.com>
2023-04-26 01:33:34 -0300

    feat: Create API constants for http status

---
+++ src/core/api/constants/index.ts
@@ -0,0 +1,12 @@
+export enum EnumHttpStatus {
+  Success = 200,
+  Created = 201,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  NotAcceptable = 406,
+  Conflict = 409,
+  PreconditionFailed = 412,
+}
