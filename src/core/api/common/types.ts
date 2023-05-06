dd85d5f9256ea785ff5293d9e4c90909847fbbab
teichx <teichx@hotmail.com>
2023-05-06 00:18:16 -0300

    feat: Create types for common api handlers

--- src/core/api/common/types.ts
+++ src/core/api/common/types.ts
@@ -1,9 +1,21 @@
-import { NextApiRequest } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export type ErrorMessage = {
   message: string;
 };
 
 export type ReqProps = {
-  req: NextApiRequest;
+  req: ReqCustomQueryProps<unknown>;
 };
+
+export type ObjectProps = Record<string, string | number | undefined | boolean>;
+
+export interface ReqCustomQueryProps<T = ObjectProps>
+  extends Omit<NextApiRequest, 'query'> {
+  query: T;
+}
+
+export type RequestHandler<TQueryProps = ObjectProps, TResponse = unknown> = (
+  req: ReqCustomQueryProps<TQueryProps>,
+  res: NextApiResponse<TResponse>
+) => void | Promise<void>;
