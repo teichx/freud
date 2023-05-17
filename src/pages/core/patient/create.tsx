8e1b6f1161dab6cdaf501102283ed775be23f443
teichx <teichx@hotmail.com>
2023-05-16 23:04:58 -0300

    feat: Create page for create patient

--- src/pages/core/patient/create.tsx
+++ src/pages/core/patient/create.tsx
@@ -1,5 +1,10 @@
+import { PatientForm } from '~/core/sections/PatientForm';
 import { AppPage } from '~/core/template/AppPage';
 
-export const CreatePatient = () => <AppPage>Create Patient</AppPage>;
+export const CreatePatient = () => (
+  <AppPage>
+    <PatientForm />
+  </AppPage>
+);
 
 export default CreatePatient;
