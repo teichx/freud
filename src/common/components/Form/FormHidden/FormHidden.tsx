bc79661a10e18f55ccbedcf4d007c60ecea83d29
teichx <teichx@hotmail.com>
2023-05-18 23:59:05 -0300

    refactor: FormHidden to react final form

--- src/common/components/Form/FormHidden/FormHidden.tsx
+++ src/common/components/Form/FormHidden/FormHidden.tsx
@@ -1,39 +1,19 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC } from 'react';
 
-import { Hide, Input } from '@chakra-ui/react';
-import { useField } from '@unform/core';
+import { Input } from '@chakra-ui/react';
+import { Field } from 'react-final-form';
 
 import { FormHiddenProps } from './types';
 
 export const FormHidden: FC<FormHiddenProps> = ({
   name,
-  defaultValue: defaultValueParam,
-}) => {
-  const inputRef = useRef(null);
-  const { fieldName, registerField, defaultValue, error } = useField(name);
-
-  useEffect(() => {
-    registerField<string | boolean>({
-      name: fieldName,
-      ref: inputRef.current,
-      getValue: (ref) => ref.value,
-      setValue: (ref, value) => {
-        ref.value = value;
-      },
-      clearValue: (ref) => {
-        ref.value = '';
-      },
-    });
-  }, [fieldName, registerField]);
-
-  return (
-    <Hide>
-      <Input
-        name={name}
-        ref={inputRef}
-        isInvalid={!!error}
-        defaultValue={defaultValueParam || defaultValue}
-      />
-    </Hide>
-  );
-};
+  defaultValue,
+  ...props
+}) => (
+  <Field
+    type='hidden'
+    name={name}
+    defaultValue={defaultValue}
+    render={({ input }) => <Input type='hidden' {...props} {...input} />}
+  />
+);
