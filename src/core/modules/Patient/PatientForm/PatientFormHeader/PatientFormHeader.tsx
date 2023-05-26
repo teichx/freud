94f58fd5b836e79323c54aef5f5e663669e192a4
teichx <teichx@hotmail.com>
2023-05-26 00:45:05 -0300

    refactor: Able save patient only if changes

--- src/core/modules/Patient/PatientForm/PatientFormHeader/PatientFormHeader.tsx
+++ src/core/modules/Patient/PatientForm/PatientFormHeader/PatientFormHeader.tsx
@@ -1,25 +1,23 @@
 import { Button, ButtonGroup, Flex } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { useFormState } from 'react-final-form';
 import { useTranslation } from 'react-i18next';
 import { FiSave, FiPrinter, FiArchive, FiShare2 } from 'react-icons/fi';
 
 import { Section } from '~/common/components/Section';
-import { Routes } from '~/core/constants';
 import { useLoader } from '~/core/services';
 
 export const PatientFormHeader = () => {
   const { isLoading } = useLoader();
   const { t } = useTranslation();
-  const { push } = useRouter();
+  const { back } = useRouter();
+  const { pristine } = useFormState();
 
   return (
     <Section disabledLoading>
       <Flex justifyContent='flex-end'>
         <ButtonGroup spacing='4' variant='solid'>
-          <Button
-            onClick={() => push(Routes.Core.Patient.List)}
-            variant='outline'
-          >
+          <Button onClick={back} variant='outline'>
             {t('words.back')}
           </Button>
 
@@ -38,6 +36,7 @@ export const PatientFormHeader = () => {
           <Button
             type='submit'
             color='white'
+            isDisabled={pristine}
             isLoading={isLoading}
             leftIcon={<FiSave />}
             colorScheme='book.desertSun'
