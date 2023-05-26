c8d46421a99a8a8c3e1a420a716be760304b666f
teichx <teichx@hotmail.com>
2023-05-26 01:32:28 -0300

    feat: SaveButton with default isLoading

--- src/common/components/Buttons/variant/SaveButton.tsx
+++ src/common/components/Buttons/variant/SaveButton.tsx
@@ -4,6 +4,8 @@ import { Button } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { FiSave } from 'react-icons/fi';
 
+import { useLoader } from '~/core/services';
+
 import { VariantButtonProps } from '../types';
 
 export const SaveButton: FC<VariantButtonProps> = ({
@@ -12,11 +14,13 @@ export const SaveButton: FC<VariantButtonProps> = ({
   ...props
 }) => {
   const { t } = useTranslation();
+  const { isLoading } = useLoader();
 
   return (
     <Button
       type='submit'
       color='white'
+      isLoading={isLoading}
       leftIcon={<FiSave />}
       colorScheme='book.desertSun'
       {...props}
