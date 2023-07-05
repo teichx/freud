1c837c65f6a676c309773e356be09907777e5dc2
teichx <teichx@hotmail.com>
2023-07-05 01:18:06 -0300

    refactor: Change file reducer initializer

--- src/common/pages/_app.tsx
+++ src/common/pages/_app.tsx
@@ -1,14 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
+import { initMessageListener } from 'redux-state-sync';
 
-import { persister, ReducerStore } from '~/common/reducer';
+import { getReducerStore } from '~/common/reducer';
 import { CustomTheme } from '~/common/themes/CustomTheme';
+
 import '~/common/locale/i18n';
 
+const store = getReducerStore();
+const persister = persistStore(store);
+
+initMessageListener(store);
+
 export const App = ({ Component, pageProps }: AppProps) => (
-  <Provider store={ReducerStore}>
+  <Provider store={store}>
     <PersistGate loading={null} persistor={persister}>
       <ChakraProvider theme={CustomTheme}>
         <Component {...pageProps} />
--- src/common/reducer/index.ts
+++ src/common/reducer/index.ts
@@ -1,48 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { combineReducers } from 'redux';
-import {
-  PERSIST,
-  persistReducer,
-  persistStore,
-  REHYDRATE,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import {
-  createStateSyncMiddleware,
-  initMessageListener,
-} from 'redux-state-sync';
+import { PERSIST, REHYDRATE } from 'redux-persist';
+import { createStateSyncMiddleware } from 'redux-state-sync';
 
-import { AuthReducer, AUTH_KEY } from '~/core/services/Auth';
-import { LOADER_KEY, LoaderReducer } from '~/core/services/Loader';
+import { rootReducer } from './rootReducer';
 
-const rootReducer = combineReducers({
-  [AUTH_KEY]: persistReducer({ storage, key: AUTH_KEY }, AuthReducer),
-  [LOADER_KEY]: LoaderReducer,
-});
+export const getReducerStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }).concat(
+        createStateSyncMiddleware({
+          blacklist: [PERSIST, REHYDRATE],
+          broadcastChannelOption: {
+            type: typeof window === 'undefined' ? 'simulate' : 'native',
+          },
+        })
+      ),
+  });
 
-export const ReducerStore = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }).concat(
-      createStateSyncMiddleware({
-        blacklist: [PERSIST, REHYDRATE],
-        broadcastChannelOption: {
-          type: typeof window === 'undefined' ? 'simulate' : 'native',
-        },
-      })
-    ),
-});
+export type RootState = ReturnType<
+  ReturnType<typeof getReducerStore>['getState']
+>;
 
-export const persister = persistStore(ReducerStore);
-
-initMessageListener(ReducerStore);
-
-export type RootState = ReturnType<typeof ReducerStore.getState>;
-
-export type AppDispatch = typeof ReducerStore.dispatch;
+export type AppDispatch = ReturnType<typeof getReducerStore>['dispatch'];
 
 type DispatchFunc = () => AppDispatch;
 
