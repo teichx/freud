bbf6d0a03655e67b18f53c73b2744f10bbc35101
teichx <teichx@hotmail.com>
2023-07-07 10:07:56 -0300

    test(component): Create test for ReadOnlyText

---
+++ tests/common/components/ReadOnlyText/ReadOnlyText.spec.tsx
@@ -0,0 +1,63 @@
+import { render } from 'test-utils';
+
+import { ReadOnlyText } from '~/common/components/ReadOnlyText';
+
+describe('component.ReadOnlyText', () => {
+  it('renders label and value', () => {
+    expect.assertions(1);
+
+    const { asFragment } = render(
+      <ReadOnlyText label='<label>' value='<value>' />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders label value and helper text', () => {
+    expect.assertions(1);
+
+    const { asFragment } = render(
+      <ReadOnlyText
+        label='<label>'
+        value='<value>'
+        helperText='<helper text>'
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('unForceHelperText', () => {
+    expect.assertions(1);
+
+    const { asFragment } = render(
+      <ReadOnlyText label='<label>' value='<value>' unForceHelperText />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('unForceHelperText difference to default', () => {
+    expect.assertions(1);
+
+    const { asFragment: unForceHelperTextFragment } = render(
+      <ReadOnlyText label='<label>' value='<value>' unForceHelperText />
+    );
+
+    const { asFragment: defaultFragment } = render(
+      <ReadOnlyText label='<label>' value='<value>' />
+    );
+
+    expect(unForceHelperTextFragment()).not.toStrictEqual(defaultFragment());
+  });
+
+  it('isTextArea noOfLines=5', () => {
+    expect.assertions(1);
+
+    const { asFragment } = render(
+      <ReadOnlyText label='<label>' value='<value>' isTextArea noOfLines={5} />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
