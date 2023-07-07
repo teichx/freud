2268e49ed0ea600b148d35034a9f69ab77a8ba79
teichx <teichx@hotmail.com>
2023-07-06 22:55:01 -0300

    test: Create test-utils to export default wrappers

---
+++ tests/utils/test-utils.tsx
@@ -0,0 +1,41 @@
+import { FC, Fragment, PropsWithChildren, ReactElement } from 'react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+const { Provider } = jest.requireActual('react-redux');
+
+import { rootReducer } from '~/common/reducer/rootReducer';
+import { CustomTheme } from '~/common/themes/CustomTheme';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+const DefaultProviders: FC<PropsWithChildren> = ({ children }) => (
+  <Provider store={store}>
+    <ChakraProvider theme={CustomTheme}>{children}</ChakraProvider>
+  </Provider>
+);
+
+export type AllOptions = RenderOptions & {
+  hocWrapper?: FC<PropsWithChildren>;
+};
+
+const customRender = (element: ReactElement, options?: AllOptions) => {
+  const Wrapper = options?.wrapper || Fragment;
+  const HocWrapper = options?.hocWrapper || DefaultProviders;
+
+  return render(element, {
+    wrapper: ({ children }) => (
+      <HocWrapper>
+        <Wrapper>{children}</Wrapper>
+      </HocWrapper>
+    ),
+    ...options,
+  });
+};
+
+export * from '@testing-library/react';
+
+export { customRender as render };
