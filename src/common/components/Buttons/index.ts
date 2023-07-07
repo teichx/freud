419304c4f6d93fc2075d1c2f8e7a766285809b7f
teichx <teichx@hotmail.com>
2023-07-06 22:53:42 -0300

    feat: Change export buttons

--- src/common/components/Buttons/index.ts
+++ src/common/components/Buttons/index.ts
@@ -1,1 +1,11 @@
 export * from './variant';
+export * from './types';
+
+import { BackButton, CancelButton, LinkButton, SaveButton } from './variant';
+
+export const Buttons = {
+  Back: BackButton,
+  Cancel: CancelButton,
+  Link: LinkButton,
+  Save: SaveButton,
+};
