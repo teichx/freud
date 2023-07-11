1c37ccf419926db94a40b2e2515d0f958a464694
teichx <teichx@hotmail.com>
2023-07-10 21:31:58 -0300

    test(component): Create tests for Logo

---
+++ tests/common/components/Logo/Logo.spec.tsx
@@ -0,0 +1,22 @@
+import { render } from 'test-utils';
+
+import { Logo, LogoSizes, LogoVariants } from '~/common/components/Logo';
+
+describe('component.Logo', () => {
+  const sizes: LogoSizes[] = ['small', 'medium', 'large'];
+  const variants: LogoVariants[] = ['icon', 'text', 'full'];
+
+  const combinations = sizes
+    .map((x) => variants.map<[LogoSizes, LogoVariants]>((y) => [x, y]))
+    .reduce((acc, current) => [...acc, ...current], []);
+
+  combinations.forEach(([size, variant]) => {
+    it(`renders size ${size} variant ${variant}`, () => {
+      expect.assertions(1);
+
+      const { asFragment } = render(<Logo size={size} variant={variant} />);
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
