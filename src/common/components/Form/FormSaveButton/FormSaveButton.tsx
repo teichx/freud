e2164c8e1f3c0756a927d4068a24a9735a69776f
teichx <teichx@hotmail.com>
2023-07-20 01:51:49 -0300

    feat: FormSaveButton with disabled reason tooltip

--- src/common/components/Form/FormSaveButton/FormSaveButton.tsx
+++ src/common/components/Form/FormSaveButton/FormSaveButton.tsx
@@ -1,12 +1,29 @@
 import { FC } from 'react';
 
 import { useFormState } from 'react-final-form';
+import { useTranslation } from 'react-i18next';
 
 import { SaveButton } from '../../Buttons';
+import { TooltipComponent } from '../../TooltipComponent';
 import { FromSaveButtonProps } from './types';
 
 export const FormSaveButton: FC<FromSaveButtonProps> = (props) => {
-  const { pristine } = useFormState();
+  const { t } = useTranslation(undefined, {
+    keyPrefix: 'form',
+  });
+  const { pristine, hasValidationErrors } = useFormState();
 
-  return <SaveButton isDisabled={pristine} {...props} />;
+  const options: [boolean, string][] = [
+    [pristine, 'disabledByPristine'],
+    [hasValidationErrors, 'disabledByHasValidationErrors'],
+  ];
+  const [, translateKey] = options.find(([condition]) =>
+    Boolean(condition)
+  ) || [false, undefined];
+
+  return (
+    <TooltipComponent label={translateKey ? t(translateKey) : undefined}>
+      <SaveButton isDisabled={pristine || hasValidationErrors} {...props} />
+    </TooltipComponent>
+  );
 };
