489db50d4d1f04fe7d5d6d9df522de6ea40dc166
teichx <teichx@hotmail.com>
2023-10-12 00:19:56 -0300

    refactor: Update objectToUniqueList more flexible

--- src/common/helpers/validation/objectList.ts
+++ src/common/helpers/validation/objectList.ts
@@ -1,15 +1,19 @@
 export const objectToUniqueList = (
-  value?: Record<string, boolean | undefined>
-): string[] =>
-  Array.isArray(value)
-    ? Array.from(new Set(value))
-    : Array.from(
-        new Set(
-          Object.entries(value || {})
-            .filter(([, value]) => !!value)
-            .map(([key]) => key)
-        )
-      );
+  value?: string | Record<string, boolean | undefined>
+): string[] => {
+  if (!value) return [];
+  if (Array.isArray(value)) return Array.from(new Set(value));
+  if (typeof value === 'object')
+    return Array.from(
+      new Set(
+        Object.entries(value || {})
+          .filter(([, value]) => !!value)
+          .map(([key]) => key)
+      )
+    );
+
+  return [value].filter(Boolean);
+};
 
 export const listToObject = (value: Set<string> | string[] | undefined) =>
   Object.fromEntries(Array.from(value || []).map((x) => [x, true]));
