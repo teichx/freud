f1aa81a227a21d50d7a6ba2faad0bd568f4d5c51
teichx <teichx@hotmail.com>
2023-10-18 20:46:09 -0300

    fix: Instrumentation run only in development

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,8 @@
 import { runAll } from './core/api/migrations';
 
 export const register = async () => {
+  if (process.env.NODE_ENV !== 'development') return;
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await runAll();
   }
