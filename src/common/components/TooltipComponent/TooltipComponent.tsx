321832f68fa3e15b890591958a4700982d36f2bc
teichx <teichx@hotmail.com>
2023-10-27 09:57:11 -0300

    refactor: TooltipComponent is ready to server

--- src/common/components/TooltipComponent/TooltipComponent.tsx
+++ src/common/components/TooltipComponent/TooltipComponent.tsx
@@ -1,13 +1,29 @@
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 
-import { StyledTooltipComponent } from './styles';
+import { Tooltip } from '@chakra-ui/react';
+
 import { TooltipComponentProps } from './types';
 
 export const TooltipComponent: FC<TooltipComponentProps> = ({
   children,
   ...props
 }) => (
-  <StyledTooltipComponent hasArrow {...props}>
-    {children}
-  </StyledTooltipComponent>
+  <Suspense>
+    <Tooltip
+      sx={{
+        borderWidth: '1px',
+        paddingTop: '1',
+        paddingLeft: '3',
+        paddingRight: '3',
+        borderRadius: 'md',
+        paddingBottom: '1',
+        fontWeight: 'semibold',
+        ...props.sx,
+      }}
+      hasArrow
+      {...props}
+    >
+      {children}
+    </Tooltip>
+  </Suspense>
 );
--- src/common/components/TooltipComponent/index.ts
+++ src/common/components/TooltipComponent/index.ts
@@ -1,3 +1,2 @@
 export * from './TooltipComponent';
-export * from './styles';
 export * from './types';
