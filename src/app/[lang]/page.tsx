28a68701b7a2631b1fb1cf1e983a72ec4d8bb2f2
teichx <teichx@hotmail.com>
2023-10-27 19:34:27 -0300

    fix: Static params on locale

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,20 @@
 import { Box } from '@chakra-ui/react';
+import { setStaticParamsLocale } from 'next-international/server';
 
 import { Footer, Header } from './_project/components';
 
-const Index = () => (
-  <Box width='100%'>
-    <Header />
+const Index = ({ params: { lang } }: { params: { lang: string } }) => {
+  setStaticParamsLocale(lang);
 
-    <Box h='50vh' />
+  return (
+    <Box width='100%'>
+      <Header />
 
-    <Footer />
-  </Box>
-);
+      <Box h='50vh' />
+
+      <Footer />
+    </Box>
+  );
+};
 
 export default Index;
