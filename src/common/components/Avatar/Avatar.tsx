00754593898e81aecb7e542259b76984f8b63f95
teichx <teichx@hotmail.com>
2023-10-30 08:57:39 -0300

    refactor: Avatar to use sx instead of styled

--- src/common/components/Avatar/Avatar.tsx
+++ src/common/components/Avatar/Avatar.tsx
@@ -1,14 +1,24 @@
 import { forwardRef } from 'react';
 
+import { Avatar as ChakraUiAvatar } from '@chakra-ui/react';
 import Image from 'next/image';
 
-import { ChakraUiAvatar } from './style';
 import { AvatarProps } from './types';
 
 export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>(
   function AvatarFunction({ name, src, w, h, ...props }, ref) {
     return (
       <ChakraUiAvatar
+        sx={{
+          borderRadius: '50%',
+          img: {
+            top: 0,
+            left: 0,
+            position: 'absolute',
+            borderRadius: '50%',
+          },
+          ...props.sx,
+        }}
         {...props}
         ref={ref}
         name={name}
