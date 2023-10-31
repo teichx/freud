9cde309a50314c02085b0b1c71318359ac0bb088
teichx <teichx@hotmail.com>
2023-10-31 11:20:43 -0300

    feat: Create Home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,16 +1,181 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Container, Divider, Heading } from '@chakra-ui/react';
 import { setStaticParamsLocale } from 'next-international/server';
+import Image from 'next/image';
 
-import { Footer, Header } from './_project/components';
+import { LinkButton } from '~/common/components/Buttons/variant/LinkButton';
+import { LoginOrAccessButton } from '~/common/components/Buttons/variant/LoginOrAccessButton';
+import { LoginOrNull } from '~/common/components/Buttons/variant/LoginOrNull';
+import { ProjectRoutes } from '~/core/constants';
+import { getScopedI18n } from '~/i18n/server';
 
-const Index = ({ params: { lang } }: { params: { lang: string } }) => {
+import { Footer, Header, ProjectSection } from './_project/components';
+
+const Index = async ({ params: { lang } }: { params: { lang: string } }) => {
   setStaticParamsLocale(lang);
+  const t = await getScopedI18n('project.home');
 
   return (
     <Box width='100%'>
       <Header />
 
-      <Box h='50vh' />
+      <Box
+        sx={{
+          w: '100%',
+          position: 'relative',
+        }}
+      >
+        <Image
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'top',
+            transform: 'scaleX(-1)',
+          }}
+          sizes='(max-width: 1300px) 600px, (max-width: 1024px) 500px, 100vw'
+          priority
+          quality={75}
+          fill
+          alt={t('welcome.alt')}
+          src='/static/project/home/pexels-cottonbro-studio-4098277.jpg'
+        />
+        <Box
+          sx={{
+            mt: '-80px',
+            backdropFilter: 'blur(5px)',
+            py: ['5%', '5%', '2.5%', '2.5%'],
+          }}
+        >
+          <Container w='100%' maxW='container.xl'>
+            <Box
+              sx={{
+                minH: '100vh',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box transform='translateY(-50%)' color='book.darkBlue.500'>
+                <Heading as='h1'>{t('welcome.title')}</Heading>
+                <Divider bgColor='book.desertSun.500' my='3' />
+
+                <Heading as='h2' size='lg'>
+                  {t('welcome.text.0')}
+                </Heading>
+              </Box>
+            </Box>
+          </Container>
+        </Box>
+      </Box>
+
+      <ProjectSection
+        title={[t('builtFor.title.0'), t('builtFor.title.1')]}
+        paragraphs={[t('builtFor.text.0')]}
+        textPosition='right'
+        sx={{
+          bg: 'blackAlpha.100',
+          _dark: {
+            bg: 'whiteAlpha.100',
+          },
+          pt: { base: '5%' },
+          pb: { sm: 0 },
+          mt: { sm: '5%', lg: '7.5%' },
+          mb: { lg: '5%' },
+        }}
+        image={{
+          src: '/static/project/home/pexels-polina-tankilevitch-5234582.jpg',
+          alt: t('builtFor.alt'),
+          sx: {
+            w: { base: '110%', md: '50%', lg: '100%' },
+            mx: { base: '-5', md: 'auto' },
+            mt: { base: '5', lg: '-15%' },
+            mb: { base: 0, lg: '-15%' },
+          },
+        }}
+      />
+
+      <ProjectSection
+        title={t('price.title')}
+        paragraphs={[
+          t('price.text.0'),
+          t('price.text.1'),
+          t('price.text.2'),
+          <Box key='divider' my='4'>
+            <Divider key='divider' bgColor='book.desertSun.500' />
+          </Box>,
+          t('price.secondText.0'),
+        ]}
+        textPosition='left'
+        image={{
+          alt: t('price.alt'),
+          src: '/static/project/home/pexels-cup-of-couple-6956772.jpg',
+        }}
+      />
+
+      <ProjectSection
+        title={t('howStart.title')}
+        paragraphs={[
+          t('howStart.text.0'),
+          t('howStart.text.1'),
+          <Box w='100%' mt='5' key='login'>
+            <LoginOrNull
+              bgColor='book.desertSun.500'
+              color='white'
+              _hover={{
+                bgColor: 'book.desertSun.600',
+              }}
+            />
+          </Box>,
+        ]}
+        textPosition='right'
+        image={{
+          src: '/static/project/home/pexels-mikhail-nilov-8653735.jpg',
+          alt: t('howStart.alt'),
+        }}
+      />
+
+      <ProjectSection
+        title={t('privacy.title')}
+        paragraphs={[
+          t('privacy.text.0'),
+          t('privacy.text.1'),
+          <Box key='toPolicy' mt='3'>
+            <LinkButton
+              color='white'
+              bgColor='book.desertSun.500'
+              href={ProjectRoutes.Policies}
+              _hover={{ bgColor: 'book.desertSun.600' }}
+            >
+              {t('privacy.cta')}
+            </LinkButton>
+          </Box>,
+        ]}
+        textPosition='left'
+        image={{
+          src: '/static/project/home/pexels-george-milton-7034717.jpg',
+          alt: t('privacy.alt'),
+        }}
+      />
+
+      <ProjectSection
+        title={t('joinUs.title')}
+        paragraphs={[
+          t('joinUs.text.0'),
+          <Divider key='divider' my='3' bgColor='book.royalBlue.500' />,
+          t('joinUs.text.1'),
+          <Box w='100%' mt='5' key='login'>
+            <LoginOrAccessButton
+              bgColor='book.royalBlue.500'
+              color='white'
+              _hover={{
+                bgColor: 'book.royalBlue.600',
+              }}
+            />
+          </Box>,
+        ]}
+        textPosition='right'
+        image={{
+          src: '/static/project/home/pexels-cottonbro-studio-4101183.jpg',
+          alt: t('joinUs.alt'),
+        }}
+      />
 
       <Footer />
     </Box>
--- src/i18n/pt-BR/project.ts
+++ src/i18n/pt-BR/project.ts
@@ -10,6 +10,57 @@ export const project = {
     Home: 'Início',
     Policies: 'Políticas',
   },
+  home: {
+    welcome: {
+      title: 'Bem-vinda ao Freud',
+      alt: 'Uma mulher mais velha, de cabelos grisalhos sentada em uma cadeira, olhando em direção ao texto de apresentação',
+      text: ['Seu espaço para o cuidado psicológico'],
+    },
+    builtFor: {
+      title: ['Construído para psicólogas,', 'pensado para o bem-estar'],
+      alt: 'Duas mulheres tendo uma conversa descontraída em um sofá',
+      text: [
+        'Você dedica sua carreira ao apoio emocional e à saúde mental de seus pacientes. Agora, damos as boas-vindas a você em nosso espaço, projetado exclusivamente para facilitar seu trabalho e aprimorar a qualidade do cuidado que oferece.',
+      ],
+    },
+    howStart: {
+      title: 'Por onde começar?',
+      alt: 'Um homem com cabelo black power loiro segurando um cartão de crédito',
+      text: [
+        'Basta fazer login com sua conta do Google, aceitar os termos de uso, e pronto! Sem a necessidade de quaiquer informação adicional.',
+        'Feito para que você tenha uma experiência tranquila e eficiente, e se concentrar-se no que mais importa: o cuidado psicológico de seus pacientes.',
+      ],
+    },
+    price: {
+      title: 'Precificação',
+      alt: 'Um homem acompanhando o desenho de uma criança, e de fundo uma estante com diversos livros',
+      text: [
+        'Atualmente estamos na versão beta, e estamos felizes em informar que não há nenhum custo associado ao uso de nossa plataforma. Você tem a oportunidade de explorar os recursos sem qualquer encargo financeiro.',
+        'Além disso, queremos garantir total transparência e respeito por sua escolha. Quando nos aproximarmos do lançamento da versão oficial, forneceremos informações detalhadas sobre a precificação com um aviso prévio de pelo menos 90 dias. Isso permitirá que você avalie as opções disponíveis e tome uma decisão informada sobre como deseja prosseguir. ',
+        'Se, por qualquer motivo, você optar por não continuar utilizando o sistema após a transição para a versão oficial, fique tranquilo. Todos os dados associados à sua conta serão enviados para o endereço de e-mail cadastrado, garantindo que você tenha o controle total de suas informações.',
+      ],
+      secondText: [
+        'Nosso compromisso é sempre colocar você, seu trabalho e a privacidade de seus pacientes em primeiro lugar.',
+      ],
+    },
+    privacy: {
+      title: 'Privacidade e segurança',
+      alt: 'Uma foto de um ambiente de trabalho, em tons leves em que aparece parcialmente uma mulher trabalhando em um notebook',
+      cta: 'Acessar nossa politica de privacidade',
+      text: [
+        'Entendemos a importância da confidencialidade na psicologia. Nós temos compromisso com a proteção dos seus dados. Todas as informações de seus pacientes são criptografadas e armazenadas de forma segura.',
+        'Além disso, utilizamos o login com o Google para adicionar uma camada adicional de segurança, garantindo que suas informações profissionais estejam protegidas enquanto você utiliza nossos serviços.',
+      ],
+    },
+    joinUs: {
+      title: 'Junte-se a Nós',
+      alt: 'Uma mulher séria sentada na cadeira, voltada completamente a lateral',
+      text: [
+        'Simplifique a gestão de seus pacientes, melhore a eficiência de sua prática e concentre-se no que mais importa: o bem-estar de seus pacientes. Comece a usar agora mesmo, e descubra como nosso serviço pode tornar sua vida profissional mais tranquila.',
+        'Este espaço é seu. Para a dedicação ao cuidado psicológico.',
+      ],
+    },
+  },
   policies: {
     inDevelopment: 'Esta página ainda está em desenvolvimento',
   },
