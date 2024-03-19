1c13528eb084e597b030f0c9ca452888e75a3cd6
teichx <teichx@hotmail.com>
2024-03-18 22:25:27 -0300

    fix: Sidebar

--- src/app/[lang]/core/_sections/BaseSidebar/components/SidebarButton/index.tsx
+++ src/app/[lang]/core/_sections/BaseSidebar/components/SidebarButton/index.tsx
@@ -2,49 +2,54 @@ import { Button, Text } from '@chakra-ui/react';
 
 import { SidebarButtonProps } from './types';
 
-export const SidebarButton = ({ label, ...props }: SidebarButtonProps) => (
-  <Button
-    size='sm'
-    sx={{
-      bg: 'transparent',
-      color: 'blackAlpha.700',
-      _selected: {
-        bg: 'whiteAlpha.800',
-        color: 'blackAlpha.800',
-        _hover: {
-          bg: 'whiteAlpha.900',
-          color: 'blackAlpha.900',
-        },
-      },
-      _hover: {
-        bg: 'whiteAlpha.800',
-        color: 'blackAlpha.800',
-        _disabled: {
-          bg: 'transparent',
-          color: 'blackAlpha.700',
-        },
-      },
-      _dark: {
-        color: 'whiteAlpha.700',
-        _hover: {
-          color: 'blackAlpha.800',
-          _disabled: {
-            color: 'whiteAlpha.700',
-          },
-        },
+export function SidebarButton<TProps = SidebarButtonProps>({
+  label,
+  ...props
+}: SidebarButtonProps & TProps) {
+  return (
+    <Button
+      size='sm'
+      sx={{
+        bg: 'transparent',
+        color: 'blackAlpha.700',
         _selected: {
+          bg: 'whiteAlpha.800',
           color: 'blackAlpha.800',
           _hover: {
+            bg: 'whiteAlpha.900',
             color: 'blackAlpha.900',
           },
         },
-      },
-      ...props.sx,
-    }}
-    {...props}
-  >
-    <Text ml='0' mr='auto'>
-      {label}
-    </Text>
-  </Button>
-);
+        _hover: {
+          bg: 'whiteAlpha.800',
+          color: 'blackAlpha.800',
+          _disabled: {
+            bg: 'transparent',
+            color: 'blackAlpha.700',
+          },
+        },
+        _dark: {
+          color: 'whiteAlpha.700',
+          _hover: {
+            color: 'blackAlpha.800',
+            _disabled: {
+              color: 'whiteAlpha.700',
+            },
+          },
+          _selected: {
+            color: 'blackAlpha.800',
+            _hover: {
+              color: 'blackAlpha.900',
+            },
+          },
+        },
+        ...props.sx,
+      }}
+      {...props}
+    >
+      <Text ml='0' mr='auto'>
+        {label}
+      </Text>
+    </Button>
+  );
+}
