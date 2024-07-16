9938e32273ec466b6d731dd52f4abefbadbdcf52
teichx <teichx@hotmail.com>
2024-07-15 21:26:52 -0300

    feat(api): Get patient by id v2

---
+++ src/app/api/patient/v2/[patientId]/(getById)/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server';
+
+import { Patient } from '~/app/api/patient/v2/model';
+import { listToObject } from '~/common/validation';
+import { sendError } from '~/core/api';
+import { getCustomerId } from '~/core/modules/Customer/auth';
+
+import { GetPatientHandler } from './types';
+
+export const GET: GetPatientHandler = async (req, ctx) => {
+  const { authError, customerId } = await getCustomerId();
+  if (!customerId) return sendError({ error: authError });
+
+  const { patientId } = ctx.params;
+  if (!patientId) return sendError({ error: 'patientId in query is required' });
+
+  const [patient] = await Patient.query({
+    PK: { eq: customerId },
+    SK: { eq: patientId },
+  }).exec();
+
+  if (!patient) {
+    return sendError({
+      error: 'Patient not found',
+      status: 'NotFound',
+    });
+  }
+
+  const { contact, personal } = patient;
+  return NextResponse.json({
+    patient: {
+      ...patient,
+      contact: {
+        ...contact,
+        address: contact?.address || personal?.address,
+        phoneNumber: contact?.phoneNumber || personal?.phoneNumber,
+        emergency: contact?.emergency || personal?.emergency,
+      },
+      symptoms: {
+        ...patient.symptoms,
+        cognitive: listToObject(patient.symptoms.cognitive),
+        emotional: listToObject(patient.symptoms.emotional),
+      },
+    },
+  });
+};
---
+++ src/app/api/patient/v2/[patientId]/(getById)/types.ts
@@ -0,0 +1,16 @@
+import { ErrorMessage, RequestQueryHandler } from '~/core/api';
+
+import { ParsedPatientFields } from '../../schema/types';
+
+export type GetPatientSuccess = {
+  patient: ParsedPatientFields | undefined;
+};
+
+export type GetPatientResponse = GetPatientSuccess | ErrorMessage;
+
+export type GetPatientQueryProps = { patientId: string };
+
+export type GetPatientHandler = RequestQueryHandler<
+  GetPatientQueryProps,
+  GetPatientResponse
+>;
