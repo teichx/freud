7587aa9e0843978df248896b0874a145039416e8
teichx <teichx@hotmail.com>
2024-07-22 22:55:56 -0300

    test: Rename root folder

--- jest.config.mjs
+++ jest.config.mjs
@@ -30,7 +30,7 @@ const config = {
     '^~/(.*)$': '<rootDir>/src/$1',
   },
 
-  testPathIgnorePatterns: ['tests/utils/*.*'],
+  testPathIgnorePatterns: ['__tests__/utils/*.*'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
